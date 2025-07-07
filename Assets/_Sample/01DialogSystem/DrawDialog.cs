60364ed552d30b56e9620012be76bcf0497074f3
ee2ssam <97661118+ee2ssam@users.noreply.github.com>
2025-07-07 13:21:44 +0900

    1. 다이알로그 시스템 구현 2. 스크립터블 오브젝트 구현

--- Assets/_Sample/01DialogSystem/DrawDialog.cs
+++ Assets/_Sample/01DialogSystem/DrawDialog.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Xml;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace My3DGame
 {
@@ -11,9 +13,21 @@ namespace My3DGame
     {
         #region Variables
         //데이터 파일 읽기
+        [SerializeField]
         private string xmlFile = "Dialog";        //데이터 파일 이름
+        private XmlNodeList allNodes;                //모든 대화 리스트
 
-        private XmlNodeList allNodes;                //XML node 리스트
+        //현재 대화
+        private Queue<Dialog> dialogs;
+
+        //UI
+        public TextMeshProUGUI nameText;
+        public TextMeshProUGUI sentenceText;
+        public GameObject nacImage;
+        public GameObject nextButton;
+
+        private bool isTyping = false;
+        private string tmpSentence = "";
         #endregion
 
         #region Unity Event Method
@@ -22,6 +36,9 @@ namespace My3DGame
             //데이터 읽기
             LoadDialogXml(xmlFile);
 
+            //초기화
+            dialogs = new Queue<Dialog>();
+            InitDialog();
         }
         #endregion
 
@@ -29,11 +46,132 @@ namespace My3DGame
         //xml 파일 읽어서 XmlNodeList에 담기
         private void LoadDialogXml(string filename)
         {
-            var xmlTextFile = Resources.Load<TextAsset>("Dialog/filename");
+            var xmlTextFile = Resources.Load<TextAsset>("Dialog/" + filename);
 
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.LoadXml(xmlTextFile.text);
             allNodes = xmlDoc.SelectNodes("root/dialog");
+        }
+        
+        //Dialog 초기화
+        private void InitDialog()
+        {
+            //Queue 비우기
+            dialogs.Clear();
+
+            //UI 초기화
+            nacImage.SetActive(false);
+            nameText.text = "";
+            sentenceText.text = "";
+
+            nextButton.SetActive(false);
+        }
+
+        //매개변수로 들어온 인덱스의 Dialog 시작하기
+        public void StartDialog(int dialogIndex)
+        {
+            //모든 노드에서 현재 대화 노드를 찾아 Queue에 저장
+            foreach (XmlNode node in allNodes)
+            {
+                int num = int.Parse(node["number"].InnerText);
+                if(num == dialogIndex)
+                {
+                    Dialog dialog = new Dialog();
+                    dialog.number = num;
+                    dialog.character = int.Parse(node["character"].InnerText);
+                    dialog.name = node["name"].InnerText;
+                    dialog.sentence = node["sentence"].InnerText;
+
+                    dialogs.Enqueue(dialog);
+                }
+            }
+
+            //다이알 로그 시작 연출
+            /*if(dialogs.Count > 0)
+            {
+
+            }*/
+
+            //첫번째 대화 보여주기
+            DrawNext();
+        }
+
+        //Queue에 있는 대화 내용을 꺼내 보여준다
+        public void DrawNext()
+        {
+            //dialogs 체크
+            if(dialogs.Count <= 0)
+            {
+                EndDialog();
+                return;
+            }
+
+            //다음 버튼 안보이기
+            nextButton.SetActive(false);
+
+            //현재 보여줄 대화를 큐에서 대화 내용 꺼내기
+            Dialog dialog = dialogs.Dequeue();
+
+            //npn 이미지 보여주기
+            if(dialog.character > 0)
+            {
+                nacImage.SetActive(true);
+                nacImage.GetComponent<Image>().sprite =
+                    Resources.Load<Sprite>("Dialog/Npc/npc0" + dialog.character.ToString());
+            }
+            else
+            {
+                nacImage.SetActive(false);
+            }
+
+            //대화캐릭터 이름
+            nameText.text = dialog.name;
+
+            //대화 내용
+            tmpSentence = dialog.sentence;
+            StartCoroutine(typingSentence(dialog.sentence));
+        }
+
+        //대화 내용 타이팅 연출 
+        IEnumerator typingSentence(string typingText)
+        {
+            isTyping = true;
+            sentenceText.text = "";
+
+            foreach (char latter in typingText)
+            {
+                sentenceText.text += latter;
+                yield return new WaitForSeconds(0.03f);
+            }
+
+            //다음 대화 버튼 보이기
+            nextButton.SetActive(true);
+            isTyping = false;
+        }
+
+        public void SkipTyping()
+        {
+            //현재 타이핑 연출
+            if (isTyping == false)
+                return;
+
+            //코루틴 종료
+            StopAllCoroutines();
+
+            sentenceText.text = tmpSentence;
+
+            //다음 대화 버튼 보이기
+            nextButton.SetActive(true);
+            isTyping = false;
+        }
+
+        //대화 종료
+        public void EndDialog()
+        {
+            //다이알 로그 초기화
+            InitDialog();
+
+            //다이알 로그 종료 연출
         }
         #endregion
     }
---
+++ Assets/_Sample/02ScriptableObject/DrawCard.cs
@@ -0,0 +1,18 @@
+using MySample;
+using UnityEngine;
+
+namespace My3DGame
+{
+    //카드 정보를 가져와서 카드를 그린다
+    public class DrawCard : MonoBehaviour
+    {
+        #region Variables
+        //카드정보
+        public Card card;       //카드 정보를 담은 스크립터블 오브젝트
+        #endregion
+
+        #region Custom Method
+        //카드 그리기
+        #endregion
+    }
+}
