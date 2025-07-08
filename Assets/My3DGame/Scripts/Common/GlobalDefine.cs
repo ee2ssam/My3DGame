e081e568bc9acbc2257cc586c72b33acab16a2e5
ee2ssam <97661118+ee2ssam@users.noreply.github.com>
2025-07-08 13:17:47 +0900

    1. 스크립터블 오브젝트 예제 2. Base Data 구현 3. 리소스 매니저 구현 4. 이펙트 클립 데이터 구현

---
+++ Assets/My3DGame/Scripts/Common/GlobalDefine.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace My3DGame.Common
+{
+    //이펙트 종류 enum값 정의
+    public enum EffectType
+    {
+        None = -1,
+        NORMAL,
+    }
+
+    //사운드 종류 enum값 정의
+    public enum SoundType
+    {
+        None = -1,
+        BGM,
+    }
+
+
+}
--- Assets/_Sample/02ScriptableObject/DrawCard.cs
+++ Assets/_Sample/02ScriptableObject/DrawCard.cs
@@ -1,7 +1,8 @@
-using MySample;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-namespace My3DGame
+namespace MySample
 {
     //카드 정보를 가져와서 카드를 그린다
     public class DrawCard : MonoBehaviour
@@ -9,10 +10,36 @@ namespace My3DGame
         #region Variables
         //카드정보
         public Card card;       //카드 정보를 담은 스크립터블 오브젝트
+
+        public TextMeshProUGUI nameText;
+        public TextMeshProUGUI descriptionText;
+        public TextMeshProUGUI manaCostText;
+        public TextMeshProUGUI attackText;
+        public TextMeshProUGUI healthText;
+
+        public Image artWork;
+        #endregion
+
+        #region Unity Event Method
+        private void Start()
+        {
+            //카드 정보 그리기
+            UpdateCard();
+        }
         #endregion
 
         #region Custom Method
-        //카드 그리기
+        //카드 그리기 - 카드정보를 UI에 연결
+        private void UpdateCard()
+        {
+            nameText.text = card.name;
+            descriptionText.text = card.description;
+            manaCostText.text = card.manaCost.ToString();
+            attackText.text = card.attack.ToString();
+            healthText.text = card.health.ToString();
+
+            artWork.sprite = card.atrImage;
+        }
         #endregion
     }
 }
