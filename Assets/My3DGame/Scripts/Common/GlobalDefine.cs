e0c2160711641fdfd7f57296497c392b0ff7434a
ee2ssam <97661118+ee2ssam@users.noreply.github.com>
2026-08-12 13:21:46 +0900

    1. 툴작업

---
+++ Assets/My3DGame/Scripts/Common/GlobalDefine.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace My3DGame
+{
+    //게임 사용하는 Enum 정의
+
+    /// <summary>
+    /// 이펙트 타입 정의
+    /// </summary>
+    public enum EffectType
+    {
+        None = -1,
+        NORMAL,
+    }
+
+    /*/// <summary>
+    /// 툴에 있는 이펙트 리스트 enum 정의
+    /// </summary>
+    public enum EffectList
+    {
+        None = -1,
+
+    }*/
+}
--- Assets/My3DGame/Scripts/GameData/BaseData.cs
+++ Assets/My3DGame/Scripts/GameData/BaseData.cs
@@ -19,15 +19,57 @@ namespace My3DGame
         public BaseData() { }
 
         //데이터 갯수 가져오기
+        public int GetDataCount()
+        {
+            //names 널 체크
+            if(this.names == null)
+                return 0;
 
+            return names.Count;
+        }
 
-        //이름 리스트 가져오기
+        //이름 리스트 가져오기 - 툴에 이름 목록 출력
+        public string[] GetNameList(bool showId, string filterWord = "")
+        {
+            int length = GetDataCount();
+            string[] returnNames = new string[length];
 
+            for (int i = 0; i < length; i++)
+            {
+                //필터링
+                if(filterWord != "")
+                {
+                    if (names[i].ToLower().Contains(filterWord.ToLower()) == false)
+                    {
+                        continue;
+                    }
+                }
 
-        //데이터 추가하기
+                //출력
+                if(showId)
+                {
+                    returnNames[i] = i.ToString() + " : " + names[i];
+                }
+                else
+                {
+                    returnNames[i] = names[i];
+                }
+            }
+
+            return returnNames;
+        }
+
+        //데이터 추가하기 - 추가 후 데이터 목록 갯수 반환
+        public virtual int AddData(string newName)
+        {
+
+            return GetDataCount();
+        }
 
         //데이터 복사하기
+        public virtual void CopyData(int index) { }
 
         //데이터 제거하기
+        public virtual void RemoveData(int index) { }
     }
 }
